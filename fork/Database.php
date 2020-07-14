a70bbc0d3b7e3b7756c0bd6fdeb29bc610bbb3ac
gashmob <kevin.traini@etu.univ-smb.fr>
2020-07-14 17:36:35 +0200

    Redesign Database class

--- fork/Database.php
+++ fork/Database.php
@@ -6,20 +6,86 @@ abstract class Database
     /**
      * @var mysqli
      */
-    private static $connection;
+    private static $mysqli;
+
+    /**
+     * @var mysqli_stmt
+     */
+    private static $stmt;
 
     public static function connect()
     {
-        self::$connection = mysqli_connect('localhost', 'root', '', '');
-
-        mysqli_set_charset(self::$connection, 'utf-8');
+        if (self::$mysqli == null) {
+            self::$mysqli = new mysqli('localhost', 'root', '', '');
+        }
     }
 
     /**
-     * @return mysqli
+     * @param string $query
+     * @return bool|mysqli_result
      */
-    public static function getConnection(): mysqli
+    public static function query(string $query)
     {
-        return self::$connection;
+        if (self::$mysqli == null) {
+            self::connect();
+        }
+
+        return self::$mysqli->query($query);
+    }
+
+    /**
+     * @param string $preparedQuery
+     * @return bool
+     */
+    public static function prepare(string $preparedQuery)
+    {
+        if (self::$stmt = self::$mysqli->prepare($preparedQuery)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param int $number
+     * @return bool
+     */
+    public static function setInt(int $number)
+    {
+        return self::$stmt->bind_param('i', $number);
+    }
+
+    /**
+     * @param float $number
+     * @return bool
+     */
+    public static function setFloat(float $number)
+    {
+        return self::$stmt->bind_param('d', $number);
+    }
+
+    /**
+     * @param string $str
+     * @return bool
+     */
+    public static function setString(string $str)
+    {
+        return self::$stmt->bind_param('s', $str);
+    }
+
+    /**
+     * @return bool
+     */
+    public static function getResult()
+    {
+        if (self::$mysqli == null) {
+            self::connect();
+        }
+
+        if (!self::$stmt == null) {
+            return self::$stmt->execute();
+        }
+
+        return false;
     }
 }
