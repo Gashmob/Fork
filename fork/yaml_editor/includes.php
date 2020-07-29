6b91d7e79687d2b16958e17931053f22ea97785d
gashmob <kevin.traini@etu.univ-smb.fr>
2020-07-29 09:28:51 +0200

    Delete includes files

--- fork/yaml_editor/includes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once 'YamlFile.php';
-include_once 'YamlArray.php';
-include_once 'YamlParser.php';
-
-include_once 'exceptions/NotYamlFileException.php';
-include_once 'exceptions/PathNotFoundException.php';
