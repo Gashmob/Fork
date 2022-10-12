7579428ba02068d8df56ec6ee0b11ea9c4556ea4
Kevin Traini <kevin@ktraini.com>
2022-10-12 12:04:08 +0200

    Add test implementation with a controller a some pages

---
+++ src/controllers/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controller;
+
+use Gashmob\Fork\AbstractController;
+use Gashmob\Fork\exceptions\RouteNotFoundException;
+use Gashmob\Fork\responses\RedirectResponse;
+use Gashmob\Fork\services\RequestService;
+
+class MainController extends AbstractController
+{
+    /**
+     * @param RequestService $request
+     * @return RedirectResponse
+     * @throws RouteNotFoundException
+     */
+    public function get(RequestService $request)
+    {
+        if ($request->getLanguage() === 'fr') {
+            return $this->redirect('/fr');
+        }
+
+        return $this->redirect('/en');
+    }
+}
