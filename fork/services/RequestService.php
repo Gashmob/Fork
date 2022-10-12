7afb0d02c5d58512e1d9f7598b8057ea2a688bca
Kevin Traini <kevin@ktraini.com>
2022-10-12 12:00:53 +0200

    Add language to RequestService

--- fork/services/RequestService.php
+++ fork/services/RequestService.php
@@ -7,25 +7,31 @@ class RequestService
     /**
      * @var string
      */
-    public $method;
+    private $method;
 
     /**
      * @var string
      */
-    public $uri;
+    private $lang;
+
+    /**
+     * @var string
+     */
+    private $uri;
 
     /**
      * @var array
      */
-    public $getParams;
+    private $getParams;
     /**
      * @var array
      */
-    public $postParams;
+    private $postParams;
 
     public function __construct()
     {
         $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
         $this->uri = isset($_GET['page']) ? '/' . $_GET['page'] : '/';
         $this->getParams = $_GET;
         unset($this->getParams['page']);
@@ -40,6 +46,14 @@ class RequestService
     public function getMethod()
     {
         return $this->method;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage()
+    {
+        return $this->lang;
     }
 
     /**
